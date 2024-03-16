e93cb55f35cce856b5ecdd0b8b3eaca6750530d5
堃源 赵 <3549778212@qq.com>
2024-03-16 14:40:00 +0800

    增加飞蛇掉落

---
+++ Items/Consumables/Flyingserpentfeather.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace kunwu.Items.Consumables
+{
+    public class Flyingserpentfeather : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.value = 450;
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 9999;
+            Item.rare = ItemRarityID.Lime;
+        }
+    }
+}
---
+++ NPCs/NewDrop.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+
+namespace kunwu.NPCs
+{
+    public class NewDrop : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.FlyingSnake)
+            {
+                npcLoot.Add(new CommonDrop(ModContent.ItemType<Items.Consumables.Flyingserpentfeather>(), chanceDenominator: 5, 2, 4, chanceNumerator: 4));
+                // 4/5=80%的概率掉落2到4个
+            }
+        }
+    }
+}
